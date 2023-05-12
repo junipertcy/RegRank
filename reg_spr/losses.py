fixed a broadcasting bug in models.py

reg_spr/losses.py
@@ -1,124 +1,14 @@
 import numpy as np
 from scipy.sparse import csr_matrix, csc_matrix
 from scipy.sparse.linalg import inv
-from scipy.linalg import svd
+
 from collections import Counter
 from itertools import combinations
 from math import comb
 from numpy.linalg import norm
 import graph_tool.all as gt
 import cvxpy as cp
-
-
-def compute_cache_from_g(
-    g, alpha, sparse=True, regularization=True, lbd=None, ell=True
-):
-    A = gt.adjacency(g)
-    # print(f"our method: adj = {A.todense()[:5,:5]}")
-    if A.shape[0] != A.shape[1]:
-        raise ValueError("Are you sure that A is symmetric?")
-    if type(A) not in [csr_matrix, csc_matrix]:
-        raise TypeError(
-            "Please make sure that A is of type `csr_matrix` or `csc_matrix` of scipy.sparse."
-        )
-    if regularization and lbd is None:
-        raise ValueError("Please assign the regularization strength (lbd).")
-    shape = A.shape[0]
-    row = []
-    col = []
-    data = []
-    row_b = []
-    col_b = []
-    data_b = []
-    for ind in zip(*A.nonzero()):
-        i, j = ind[0], ind[1]
-        if j <= i:  # TODO: double check if equality should be here??
-            _row = i * (shape - 1) + j
-        else:
-            _row = i * (shape - 1) + j - 1
-        row.append(_row)
-        row.append(_row)
-        col.append(i)
-        col.append(j)
-        data.append(A[ind] ** 0.5)
-        data.append(-A[ind] ** 0.5)
-
-        row_b.append(_row)
-        col_b.append(0)
-        data_b.append(-alpha * A[ind] ** 0.5)
-        # print(f"adding... {_row, i, A[ind] ** 0.5} & {_row, j, - A[ind] ** 0.5}")
-
-    if regularization:
-        row += list(range(shape**2 - shape, shape**2))
-        col += list(range(shape))
-        data += [lbd] * shape
-        row_b += list(range(shape**2 - shape, shape**2))
-        col_b += [0] * shape
-        data_b += [0] * shape
-        B = csr_matrix((data, (row, col)), shape=(shape**2, shape), dtype=np.float64)
-        b = csr_matrix(
-            (data_b, (row_b, col_b)), shape=(shape**2, 1), dtype=np.float64
-        )
-    else:
-        B = csr_matrix(
-            (data, (row, col)), shape=(shape**2 - shape, shape), dtype=np.float64
-        )
-        b = csr_matrix(
-            (data_b, (row_b, col_b)), shape=(shape**2 - shape, 1), dtype=np.float64
-        )
-
-    if not sparse:
-        B = B.todense()
-        b = b.todense()
-
-    if ell:
-        _ell = compute_ell(g)
-    else:
-        _ell = None
-
-    Bt_B_inv = compute_Bt_B_inv(B, sparse=0)
-
-    _, s, Vh = svd(B.todense(), full_matrices=False)
-    Bt_B_invSqrt = Vh.T @ np.diag(1 / s) @ Vh
-
-    return {
-        "B": B,
-        "b": b,
-        "ell": _ell,
-        "Bt_B_inv": Bt_B_inv,
-        "Bt_B_invSqrt": Bt_B_invSqrt,
-    }
-
-
-def compute_Bt_B_inv(B, sparse=True):
-    if not sparse:
-        B = B.todense()
-        return np.linalg.inv(np.dot(B.T, B))
-    return inv(np.dot(B.T, B))
-
-
-def grad_g_star(B, b, v):
-    return np.dot(compute_Bt_B_inv(B), v + np.dot(B.T, b))
-
-
-def compute_ell(g, arg="class"):
-    ctr_classes = Counter(g.vp[arg])
-    len_classes = len(ctr_classes)
-    # print(f"ctr_classes: {ctr_classes}")
-    comb_classes = combinations(ctr_classes, 2)
-    mb = list(g.vp[arg])
-    ell = np.zeros([comb(len_classes, 2), len(g.get_vertices())])
-
-    for idx, (i, j) in enumerate(comb_classes):
-        for vtx in g.vertices():
-            vtx = vtx.__int__()
-            if mb[vtx] == i:
-                ell[idx][vtx] = -ctr_classes[i] ** -1
-            elif mb[vtx] == j:
-                ell[idx][vtx] = ctr_classes[j] ** -1
-            else:
-                ell[idx][vtx] = 0
-    return ell
+from reg_spr.utils import compute_cache_from_g
 
 
 class Loss:
@@ -169,7 +59,7 @@ class sum_squared_loss(Loss):
     def setup(self, data, alpha):
         # data is graph_tool.Graph()
         cache = compute_cache_from_g(
-            data, alpha=alpha, sparse=1, lbd=1, ell=self.compute_ell
+            data, alpha=alpha, sparse=1, lambd=1, ell=self.compute_ell
         )
         self.B = cache["B"]
         self.b = cache["b"]
@@ -244,7 +134,7 @@ class sum_squared_loss_conj(Loss):
         # return sum((theta @ data["B"] - data["Y"]) ** 2)
 
     def setup(self, data, alpha):
-        cache = compute_cache_from_g(data, alpha=alpha, sparse=1, lbd=1)
+        cache = compute_cache_from_g(data, alpha=alpha, sparse=1, lambd=1)
         self.B = cache["B"]
         self.b = cache["b"]
         self.ell = cache["ell"]

reg_spr/models.py
@@ -46,7 +46,7 @@ class SpringRank:
             adj = gt.adjacency(data)
         else:
             raise NotImplementedError
-        print(f"bicgstab: adj = {adj.todense()[:5,:5]}")
+        # print(f"bicgstab: adj = {adj.todense()[:5,:5]}")
         ranks = self.get_ranks(adj)
 
         info = {"rank": ranks}
@@ -130,21 +130,25 @@ class SpringRank:
         if alpha == 0:
             rank = self.csr_SpringRank(A)
         else:
-            print("Running bicgstab to solve Ax=b ...")
+            if type(A) == np.ndarray:
+                A = scipy.sparse.csr_matrix(A)
+            # print("Running bicgstab to solve Ax=b ...")
+            # print("adj matrix A:\n", A.todense())
             N = A.shape[0]
-            k_in = np.sum(A, 0)
-            k_out = np.sum(A, 1)
+            k_in = scipy.sparse.csr_matrix.sum(A, 0)
+            k_out = scipy.sparse.csr_matrix.sum(A, 1).T
+
+            k_in = scipy.sparse.diags(np.array(k_in)[0])
+            k_out = scipy.sparse.diags(np.array(k_out)[0])
 
             C = A + A.T
-            D1 = np.diag(k_out + k_in)
+            D1 = k_in + k_out
+
             B = k_out - k_in
-            # print(B)
             B = B @ np.ones([N, 1])
-            # print(B)
-            print(f"alpha = {alpha}")
-            A = alpha * np.eye(N) + D1 - C
-            A = scipy.sparse.csr_matrix(np.matrix(A))
-            # print(f"shape of A: {A.shape}; shape of B: {B.shape};")
+
+            A = alpha * scipy.sparse.eye(N) + D1 - C
+
             rank = scipy.sparse.linalg.bicgstab(A, B)[0]
 
         return np.transpose(rank)